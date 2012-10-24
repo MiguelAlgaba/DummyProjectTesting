0f2ae26535257b2722e0abc43fe7af15beaa956a
Miguel Algaba <miguel.algaba.borrego@gmail.com>
2012-10-24 10:47:23 +0200

    First dummy commit

---
+++ fail.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+
+int main()
+{
+    if(1==0)
+    {
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        return EXIT_SUCCESS;
+    }
+
+}
---
+++ pass.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+
+int main()
+{
+    if(1==1)
+    {
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        return EXIT_FAILURE;
+    }
+
+}
